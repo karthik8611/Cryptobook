21bc470b376db8db5d1a3edbd2b9d0f88d823b0d
karthik8611 <mkarthik6768@gmail.com>
2023-12-02 14:41:05 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
     <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
-              <a href="https://github.com/karthik8611" className="flex items-center">
+              <a href="https://github.com/karthik8611/" className="flex items-center">
                   <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 me-3" alt="FlowBite Logo" />
                   <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Cryptobook</span>
               </a>
@@ -19,10 +19,10 @@ const Footer = () => {
                   <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
                   <ul className="text-gray-500 dark:text-gray-400 font-medium">
                       <li className="mb-4">
-                          <a href="https://flowbite.com/" className="hover:underline">Cryptobook</a>
+                          <a href="https://github.com/karthik8611/" className="hover:underline">Cryptobook</a>
                       </li>
                       <li>
-                          <a href="https://tailwindcss.com/" className="hover:underline">Tailwind CSS</a>
+                          <a href="https://github.com/karthik8611/" className="hover:underline">Tailwind CSS</a>
                       </li>
                   </ul>
               </div>
@@ -30,10 +30,10 @@ const Footer = () => {
                   <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
                   <ul className="text-gray-500 dark:text-gray-400 font-medium">
                       <li className="mb-4">
-                          <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Github</a>
+                          <a href="https://github.com/karthik8611" className="hover:underline ">Github</a>
                       </li>
                       <li>
-                          <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Discord</a>
+                          <a href="https://github.com/karthik8611" className="hover:underline">Discord</a>
                       </li>
                   </ul>
               </div>
